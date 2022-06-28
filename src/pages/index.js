0d385755ec763928d05fb93e4f2942bb463048cb
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-06-28 15:19:10 -0400

    feat: play a sound when click a button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,38 @@
+import Head from 'next/head';
+import drumPad from '../utils/drumPads.json';
+
+function Button({ instrument }) {
+  function play() {
+    let audio = document.getElementById(instrument.key);
+    audio.play();
+  }
+  addEventListener;
+
+  return (
+    <>
+      <button
+        onClick={play}
+        className="m-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-1 rounded"
+      >
+        {instrument.key}
+      </button>
+      <audio id={instrument.key} src={instrument.url}></audio>
+    </>
+  );
+}
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-5xl text-center">Hello World!</h1>
+      <Head>
+        <title>Drum Kit Machine </title>
+      </Head>
+      <h1 className="m-8 text-center text-4xl">Drum Kit Machine</h1>
+      <div className="grid   grid-cols-3 lg:gap-4 lg:mx-48 content-center">
+        {drumPad.map((instrument) => (
+          <Button key={instrument.key} instrument={instrument} />
+        ))}
+      </div>
     </>
   );
 }
